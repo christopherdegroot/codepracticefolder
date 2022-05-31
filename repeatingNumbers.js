0a35770eacb8703f2999c761331bec0d234096f3
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-05-31 11:18:55 -0700

    add final solution

--- repeatingNumbers.js
+++ repeatingNumbers.js
@@ -1,9 +1,13 @@
 const repeatNumbers = function(data) {
-  data.forEach((arr)=>{
+  let finalString = []
+  for (const arr of data) {
+    let pushString = []
     for (let i = 0; i < arr[1]; i++) {
-      console.log(arr[0])
+      pushString.push(arr[0])
     }
-  })
+    finalString.push(pushString.join(''))
+  }
+  return finalString.join(',')
 };
 
 console.log(repeatNumbers([[1, 10]]));
